533d0bf140f8cab4efca77876ed587e76b955777
jamesbaroyofficial <146837518+jamesbaroyofficial@users.noreply.github.com>
2026-09-23 19:01:59 +0800

    Update onEvent.js

--- scripts/events/onEvent.js
+++ scripts/events/onEvent.js
@@ -12,22 +12,8 @@ module.exports = {
 	onStart: async ({ api, args, message, event, threadsData, usersData, dashBoardData, threadModel, userModel, dashBoardModel, role, commandName }) => {
 		for (const item of allOnEvent) {
 			if (typeof item === "string")
-				continue;
-
-			item.onStart({
-				api,
-				args,
-				message,
-				event,
-				threadsData,
-				usersData,
-				threadModel,
-				dashBoardData,
-				userModel,
-				dashBoardModel,
-				role,
-				commandName
-			});
+				continue; // Skip if item is string, because it is the command name and is executed at ../../bot/handler/handlerEvents.js
+			item.onStart({ api, args, message, event, threadsData, usersData, threadModel, dashBoardData, userModel, dashBoardModel, role, commandName });
 		}
 	}
 };
